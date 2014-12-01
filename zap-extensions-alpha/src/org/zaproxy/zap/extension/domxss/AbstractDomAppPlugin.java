aebf4a3bb5b914d5932262a05f143d39e27d03ef
Aabha Biyani <aabha.biyani@gmail.com>
2014-11-30 22:02:41 -0800

    Incorporated few comments

--- zap-extensions-alpha/src/org/zaproxy/zap/extension/domxss/AbstractDomAppPlugin.java
+++ zap-extensions-alpha/src/org/zaproxy/zap/extension/domxss/AbstractDomAppPlugin.java
@@ -1,9 +1,50 @@
 package org.zaproxy.zap.extension.domxss;
 
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.parosproxy.paros.core.scanner.AbstractAppPlugin;
 
 public abstract class AbstractDomAppPlugin extends AbstractAppPlugin {
 
+	private static Logger log = Logger.getLogger(AbstractDomAppPlugin.class);
+	
+	private static final int LOCATION_HASH = 0;
+    private static final int LOCATION = 1;
+    private static final int LOCATION_SEARCH = 2;
+    private static final int REFERRER = 3;
+    
+    private static final int [] sources = {LOCATION_HASH, LOCATION, LOCATION_SEARCH, REFERRER};
+  
+    public static String [] locationHashAttackStrings = {
+		"#alert(1)",//if eval sink
+		"#<script>alert(1)</script>",
+		"#<img src=\"random.gif\" onerror=alert(1)>",
+		"#abc#<script>alert(1)</script>", // If document.write is the sink
+		"#abc#<img src='random.gif' onerror=alert(1)",
+		"#javascript:alert(1)"
+		};
+    
+    public static String [] locationAttackStrings = {
+    	"#<script>alert(1)</script>",
+    	"?name=abc#<img src=\"random.gif\" onerror=alert(1)>",
+    	"#<img src=\"random.gif\" onerror=alert(1)>",
+    };
+    
+    public static String [] locationSearchAttackStrings = {
+    	"?name=<img src=\"random.gif\" onerror=alert(1)>"
+    };
+   
+    public static String [] referrerAttackStrings = {
+    	"?name=<img src=\"random.gif\" onerror=alert(1)>"
+    };
+    
+    public static ArrayList<String[]> attackVectors;
 	@Override
 	public int getCategory() {
 		// TODO Auto-generated method stub
@@ -55,7 +96,81 @@ public abstract class AbstractDomAppPlugin extends AbstractAppPlugin {
 	@Override
 	public void scan() {
 		// TODO Auto-generated method stub
-
+		attackVectors = new ArrayList<String[]>();
+		attackVectors.add(locationHashAttackStrings);
+		attackVectors.add(locationAttackStrings);
+		attackVectors.add(locationSearchAttackStrings);
+		attackVectors.add(referrerAttackStrings);
+		
+		for(int source : sources)
+		{
+			try
+			{
+				scan(source);
+			}
+			catch(UnhandledAlertException uae)
+			{
+				break;
+			}
+		}
 	}
-
+	
+	public void scan(int source) throws UnhandledAlertException
+	{
+		for(String attackVector : attackVectors.get(source))
+		{
+			ArrayList<WebDriver> drivers = new ArrayList<WebDriver>();
+		/*	WebDriver firefoxDriver = new FirefoxDriver();
+			drivers.add(firefoxDriver);
+			try
+			{
+				WebDriver chromeDriver = new ChromeDriver();
+				drivers.add(chromeDriver);
+			}
+			catch(IllegalStateException ise)
+			{
+				log.debug("Chrome Web Driver property is not set.", ise);
+			}
+			try
+			{
+				WebDriver ieDriver = new InternetExplorerDriver();
+				drivers.add(ieDriver);
+			}
+			catch(IllegalStateException ise)
+			{
+				log.debug("Internet Explorer Web Driver property is not set.", ise);
+			}*/
+			try
+			{
+				scan(source, drivers, attackVector);
+			}
+			catch(UnhandledAlertException uae)
+			{
+				throw uae;
+			}
+		}
+	}
+	
+	public void scan(int source, ArrayList<WebDriver> drivers, String attackVector) throws UnhandledAlertException
+	{
+/*		for(int i = 0; i < drivers.size(); i++)
+		{
+			WebDriver driver = drivers.get(i);
+			try
+			{
+				scan(source, driver, attackVector);
+			}
+			catch(UnhandledAlertException uae)
+			{
+				throw uae;
+				
+			}
+		}
+	*/}
+	/*
+	public void scan(int source, WebDriver driver, String attackVector) throws UnhandledAlertException
+	{
+		
+	}
+	*/
 }
--- zap-extensions-alpha/src/org/zaproxy/zap/extension/domxss/TestDomXSS.java
+++ zap-extensions-alpha/src/org/zaproxy/zap/extension/domxss/TestDomXSS.java
@@ -2,7 +2,6 @@ package org.zaproxy.zap.extension.domxss;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
@@ -13,8 +12,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.parosproxy.paros.Constant;
-import org.parosproxy.paros.core.scanner.AbstractAppPlugin;
 import org.parosproxy.paros.core.scanner.Alert;
 import org.parosproxy.paros.core.scanner.Category;
 import org.parosproxy.paros.network.HttpMessage;
@@ -25,37 +22,7 @@ public class TestDomXSS extends AbstractDomAppPlugin
 {
 	private static Vulnerability vuln = Vulnerabilities.getVulnerability("wasc_8");
     private static Logger log = Logger.getLogger(TestDomXSS.class);
-    private static final int LOCATION_HASH = 1;
-    private static final int LOCATION = 2;
-    private static final int LOCATION_SEARCH = 3;
-    private static final int REFERRER = 4;
-    
-//    private static final int [] sources = {LOCATION_HASH, LOCATION, LOCATION_SEARCH, REFERRER};
-    private static final int [] sources = {REFERRER};
-  
-    public static String [] locationHashAttackStrings = {
-		"alert(1)",//if eval sink
-		"<script>alert(1)</script>",
-		"<img src=\"random.gif\" onerror=alert(1)>",
-		"abc#<script>alert(1)</script>", // If document.write is the sink
-		"abc#<img src='random.gif' onerror=alert(1)",
-		"javascript:alert(1)"
-		};
-    
-    public static String [] locationAttackStrings = {
-    	"#<script>alert(1)</script>",
-    	"?name=abc#<img src=\"random.gif\" onerror=alert(1)>",
-    	"#<img src=\"random.gif\" onerror=alert(1)>",
-    };
-    
-    public static String [] locationSearchAttackStrings = {
-    	"?name=<img src=\"random.gif\" onerror=alert(1)>"
-    };
-   
-    public static String [] referrerAttackStrings = {
-    	"?name=<img src=\"random.gif\" onerror=alert(1)>"
-    };
-    
+ 
     @Override
     public int getId() {
         return 10049;
@@ -113,261 +80,128 @@ public class TestDomXSS extends AbstractDomAppPlugin
     @Override
     public void init() {
     }
-    
+ /*   
     @Override
 	public void scan()
-	{
-    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\IEUser\\Downloads\\ZAP\\workspace_zap\\workspace-zap\\zaproxy\\lib\\chromedriver.exe");
-    	System.setProperty("webdriver.ie.driver", "C:\\Users\\IEUser\\Downloads\\ZAP\\workspace_zap\\workspace-zap\\zaproxy\\lib\\IEDriverServer.exe");
-    	
-    	HttpMessage msg = getBaseMsg();
-    	String url = msg.getRequestHeader().getURI().toString();
-    	String currURL = new String();
-    	String attackVector = new String();
-    	
-    	WebDriver firefoxDriver = null;
-    	WebDriver chromeDriver = null;
-    	WebDriver ieDriver = null;
-    	 	
-    	for(int source : sources)
-    	{
-    		switch(source)
-    		{
-    			case LOCATION_HASH:
-    				for(String attackStr : locationHashAttackStrings)
-    				{
-    					currURL = url + "#" + attackStr;
-    					
-    					firefoxDriver = new FirefoxDriver();
-    					try
-    					{
-    						scanHelperLocationHash(firefoxDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in firefox", msg);
-    						return;
-    					}
-    					finally
-    					{
-    						firefoxDriver.close();
-    					}
-    					
-    					chromeDriver = new ChromeDriver();
-    					try
-    					{
-    						scanHelperLocationHash(chromeDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Chrome", msg);
-    						return;
-    					}
-    					finally
-    					{
-    						chromeDriver.close();
-    					}
-    					
-    					ieDriver = new InternetExplorerDriver();
-    					try
-    					{
-    						scanHelperLocationHash(ieDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Internet Explorer", msg);
-    						return;
-    					}
-    					finally
-    					{
-    						ieDriver.close();
-    					}
-    				}
-    				break;
-    			case LOCATION_SEARCH:
-    				for(String attackStr : locationSearchAttackStrings)
-    				{
-    					currURL = url + attackStr;
-    					
-    					firefoxDriver = new FirefoxDriver();
-    					try
-    					{
-    						scanHelperLocationHash(firefoxDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in firefox", msg);
-    						return;
-    					}
-    					finally
-    					{
-    						firefoxDriver.close();
-    					}
-    					
-    					chromeDriver = new ChromeDriver();
-    					try
-    					{
-    						scanHelperLocationHash(chromeDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Chrome", msg);
-    						return;
-    					}
-    					finally
-    					{
-    						chromeDriver.close();
-    					}
-    					
-    					ieDriver = new InternetExplorerDriver();
-    					try
-    					{
-    						scanHelperLocationHash(ieDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Internet Explorer", msg);
-    						return;
-    					}
-    					finally
-    					{
-    						ieDriver.close();
-    					}
-    				}
-    				break;
-    			case LOCATION:
-    				for(String attackStr : locationAttackStrings)
-    				{
-    					currURL = url + attackStr;
-    					
-    					firefoxDriver = new FirefoxDriver();
-    					try
-    					{
-    						scanHelperLocationHash(firefoxDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in firefox", msg);
-    						return;
-    					}
-    					finally
-    					{
-    						firefoxDriver.close();
-    					}
-    					
-    					chromeDriver = new ChromeDriver();
-    					try
-    					{
-    						scanHelperLocationHash(chromeDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Chrome", msg);
-    						return;
-    					}
-    					finally
-    					{
-    						chromeDriver.close();
-    					}
-    					
-    					ieDriver = new InternetExplorerDriver();
-    					try
-    					{
-    						scanHelperLocationHash(ieDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Internet Explorer", msg);
-    						return;
-    					}
-    					finally
-    					{
-    						ieDriver.close();
-    					}
-    				}
-    				break;
-    			case REFERRER:
-    				for(String attackStr : referrerAttackStrings)
-    				{
-    					currURL = url + attackStr;
-    					
-    					firefoxDriver = new FirefoxDriver();
-    					try
-    					{
-    						scanHelperLocationHash(firefoxDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in firefox", msg);
-    						//return;
-    					}
-    					finally
-    					{
-    						firefoxDriver.close();
-    					}
-    					
-    					chromeDriver = new ChromeDriver();
-    					try
-    					{
-    						scanHelperLocationHash(chromeDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Chrome", msg);
-    						//return;
-    					}
-    					finally
-    					{
-    						chromeDriver.close();
-    					}
-    					
-    					ieDriver = new InternetExplorerDriver();
-    					try
-    					{
-    						scanHelperLocationHash(ieDriver, attackStr, currURL);
-    					}
-    					catch(UnhandledAlertException uae)
-    					{
-    						bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Internet Explorer", msg);
-    						//return;
-    					}
-    					finally
-    					{
-    						ieDriver.close();
-    					}
-    				}
-    				break;
-    		}
-    	}
-	}
+	{  
     
- //   private void scanHelperReferrer(WebDriver, String attackVector, String url) throws Un
-    private void scanHelperLocationHash(WebDriver driver, String attackVector, String url) throws UnhandledAlertException
+	}
+   */ 
+    private void scanHelper(WebDriver driver, String attackVector, String url) throws UnhandledAlertException
 	{
 		driver.get(url);
 		List<WebElement> inputElements = driver.findElements(By.tagName("input"));
-		for(WebElement element : inputElements)
+		//for(WebElement element : inputElements)
+		for(int i = 0; i < inputElements.size(); i++)
 		{
-			driver.get(url);
+			//driver.get(url);
+			WebElement element = inputElements.get(i);
 			element.sendKeys(attackVector);
 			element.click();
+			driver.get(url);
+			inputElements = driver.findElements(By.tagName("input"));
+			
 		}
 		List<WebElement>allElements = driver.findElements(By.tagName("div"));
-		System.out.println(allElements.size());
-		for(WebElement element : allElements)
+	//	for(WebElement element : allElements)
+		for(int i = 0; i < allElements.size(); i++)
 		{
-			System.out.println("Now clicking on " + element.getTagName());
+			WebElement element = allElements.get(i);
 			try
 			{
-				driver.get(url);
+				//driver.get(url);
 				element.click();
+				driver.get(url);
+				allElements = driver.findElements(By.tagName("div"));
 			}
 			catch(ElementNotVisibleException enve)
 			{
-				System.out.println("Element not visible exception encountered");
+				log.debug("Element not visible exception encountered");
 			}
 		}
 	}
+	
+    
+    @Override
+    public void scan(int source, ArrayList<WebDriver> drivers, String attackVector) throws UnhandledAlertException
+    {
+    	HttpMessage msg = getBaseMsg();
+    	String url = msg.getRequestHeader().getURI().toString();
+    	String currURL = url + attackVector;
+    	WebDriver firefoxDriver = new FirefoxDriver();
+    	try
+		{
+			scanHelper(firefoxDriver, attackVector, currURL);
+		}
+		catch(UnhandledAlertException uae)
+		{
+			bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Firefox ", msg);
+			//return;
+			throw uae;
+		}
+		finally
+		{
+			firefoxDriver.close();
+		}
+    	if(System.getProperty("webdriver.chrome.driver") != null)
+    	{
+    		WebDriver chromeDriver = new ChromeDriver();
+        	try
+    		{
+    			scanHelper(chromeDriver, attackVector, currURL);
+    		}
+    		catch(UnhandledAlertException uae)
+    		{
+    			bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Chrome ", msg);
+    			//return;
+    			throw uae;
+    		}
+    		finally
+    		{
+    			chromeDriver.close();
+    		}
+    	}
+    	if(System.getProperty("webdriver.ie.driver") != null)
+    	{
+    		WebDriver ieDriver = new InternetExplorerDriver();
+        	try
+    		{
+    			scanHelper(ieDriver, attackVector, currURL);
+    		}
+    		catch(UnhandledAlertException uae)
+    		{
+    			bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + " in Internet Explorer ", msg);
+    			//return;
+    			throw uae;
+    		}
+    		finally
+    		{
+    			ieDriver.close();
+    		}
+    	}
+    }
+ /*   public void scan(int source, WebDriver driver, String attackVector) throws UnhandledAlertException
+    {
+    	HttpMessage msg = getBaseMsg();
+    	String url = msg.getRequestHeader().getURI().toString();
+    	String currURL = url + attackVector;
+    	try
+		{
+			scanHelper(driver, attackVector, currURL);
+		}
+		catch(UnhandledAlertException uae)
+		{
+			bingo(Alert.RISK_MEDIUM, Alert.RISK_MEDIUM, url, null, attackVector, "", currURL + driver.getClass().getCanonicalName(), msg);
+			//return;
+			throw uae;
+		}
+		finally
+		{
+			driver.close();
+		}
+    }
+   */ 
+    
 	@Override
 	public int getRisk() {
 		return Alert.RISK_MEDIUM;
